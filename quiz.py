b876ef7fae5b52fc06ccf50a0088dd136810cfeb
Rasquin <yannymk@hotmail.com>
2019-04-02 13:27:18 +0000

    seek() method in quiz.py

--- quiz.py
+++ quiz.py
@@ -1,5 +1,7 @@
 #define a variable f and it will open our data txt file for (r)reading
+# 'r' or read mode only allows you to read from a file and if it doesn't exist, then it will throw an error
 #f = open('data.txt', 'r') ----> case data in the same directory
+#f = open('data.txt', 'r') == f = open('data.txt') because it will open only to be read.
 # relative path--> it identifies the location of the data file relative to the script that's currently running.
 #f = open('files/relative_data.txt', 'r')
 
@@ -18,3 +20,9 @@ f.close()
 
 #and then we'll print the results to screen.
 print(lines)
+
+
+#seek(offset, from_where)
+# The offset, we are already familiar with. from_where is: 0 (default) offset
+# from the beginning of the file; 1 offset from the current file pointer or
+# 2 offset from the end of the file. So seek(0,2) moves us to the end of the file
--- write.py
+++ write.py
@@ -1,10 +1,12 @@
 """ 
-# Here a new file will be created. the w is fro write. 
+# Here a new file will be created. the w is for write. 
+#The 'w'mode only allows you to write to a file and if that file doesn't exist, then a new one will be created. 
 f=open('newfile.txt', 'w')
 f.write("Hello\n") #/n for new line
 f.close()
 
 #If we change the "w" for an "a"(apendice), it will add whatever your put on write to the newfile.txt already existing.
+# The 'a' allows you to append to a file, which means to write new content after the existing content. Again, if it doesn't exist a new file will be created. 
 f=open('newfile.txt', 'a')
 f.write("World")
 f.close()
@@ -15,3 +17,17 @@ lines=['Hello', 'World', 'Welcome', 'To', 'File IO']
 text='\n'.join(lines)
 f.writelines(text)
 f.close()
+
+"""
+'r+' mode is for read and write. This allows you to read and write to a file. If it doesn't exist
+it will throw an error. If it does exist, then its contents will be overwritten.
+
+'w+' for read and write again is the same as 'r+', but a new file will be created if it
+doesn't exist instead of an error being thrown. 
+
+ 'a+' is for read and append. This is the same as 'w+', but the file is opened for append, so the existing content won't
+be overwritten. 
+
+'rb', 'wb' and 'ab' are exactly the same as 'r', 'w' and 'a', but for binary data
+instead of text. Now, you'll normally use 'r', 'w' and 'a', but since 'r' is used most
+"""
